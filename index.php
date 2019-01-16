3e5600a64e045c36ff2e5029987b280c432997c3
Michael Treanor <skeptycal@gmail.com>
2019-01-16 00:43:10 -0600

    initial commit

--- index.php
+++ index.php
@@ -1,25 +1,30 @@
+#!/usr/bin/env php
+<?php
+require __DIR__ . '/vendor/autoload.php';
+?>
 <!DOCTYPE php>
 
 <html lang="en-US">
 
 <head>
-<?php
-	$title_string = 'Michael Treanor';
-	require('/src/header_main.php');
-?>
+    <?php
+		$title_string = 'Michael Treanor';
+		require('/src/header_main.php');
+	?>
 </head>
 
 <body>
-	<section class="galaxy-wrapper">
-		<canvas id="galaxy" width="100%" height="100%"></canvas>
-		<section class="main">
-			<br /><br /><br />
-			<h1>Michael Treanor</h1>
-			<h2>skeptic | educator | maker | parent | curious | creative | introvert</h2>
-		</section>
-		<?php
+    <section class="galaxy-wrapper">
+        <canvas id="galaxy" width="100%" height="100%"></canvas>
+        <section class="main">
+            <br /><br /><br />
+            <h1>Michael Treanor</h1>
+            <h2>skeptic | educator | maker | parent | curious | creative | introvert</h2>
+        </section>
+        <?php
 			require('/src/footer_main.php')
 		?>
-		</section>
-	</body>
+    </section>
+</body>
+
 </html>
